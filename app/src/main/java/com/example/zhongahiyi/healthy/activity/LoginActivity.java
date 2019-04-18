92b6378527387e56125c5505a8ffca6defdebb67
Wprofessor <wprofessor@gmail.com>
2019-04-18 15:56:34 +0800

    merge

--- app/src/main/java/com/example/zhongahiyi/healthy/activity/LoginActivity.java
+++ app/src/main/java/com/example/zhongahiyi/healthy/activity/LoginActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-
 import com.example.zhongahiyi.healthy.R;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener,TextWatcher {
